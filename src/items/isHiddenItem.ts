2fb55e2c442c2031341666f62d03c1e927013956
Alex Menor <alex.menor@icloud.com>
2021-08-24 16:11:08 +0200

    simplify isHiddenItem function

--- src/items/isHiddenItem.ts
+++ src/items/isHiddenItem.ts
@@ -3,5 +3,5 @@
  * @returns If its or not considered hidden
  */
 export default function isHiddenItem(item: { name: string }): boolean {
-  return !!item.name.match(/^\..*$/);
+  return !!item.name.match(/^\./);
 }
