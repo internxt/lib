6040743c282ccf6162ed88ff56c57572df0f9cc8
Carlos Sala Samper <carlos15695@hotmail.com>
2021-09-28 11:09:34 +0200

    Modified email validation tests

--- test/auth/isValidEmail.test.ts
+++ test/auth/isValidEmail.test.ts
@@ -7,13 +7,6 @@ describe('isValidEmail tests', () => {
     expect(isValidEmail('test@internxt.com')).toBe(true);
   });
 
-  it('should consider invalid an email with uppercase letters', () => {
-    expect(isValidEmail('TEST@internxt.com')).toBe(false);
-    expect(isValidEmail('test@INTERNXT.com')).toBe(false);
-    expect(isValidEmail('test@internxt.COM')).toBe(false);
-    expect(isValidEmail('TEST@INTERNXT.COM')).toBe(false);
-  });
-
   it('should consider valid an email with allowed symbols', () => {
     expect(isValidEmail("test-symbols45!#$%&'*+-/=?^_`{|@internxt.com")).toBe(true);
   });
