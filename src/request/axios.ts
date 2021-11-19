a16b8331b5d3ce5d5591d231e1c094b420baa511
Sergio Gutiérrez Villalba <gv.sergio@gmail.com>
2021-11-19 12:16:06 +0100

    axios error message extractor

---
+++ src/request/axios.ts
@@ -0,0 +1,30 @@
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+type AxiosError = any;
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+type AxiosResponse<T> = any;
+type ErrorMessage = string;
+
+/**
+ * Extracts message from axios error
+ *
+ * @param err Axios error
+ * @returns Extracted error message from axios error
+ */
+export function extractMessageFromError(err: AxiosError): ErrorMessage {
+  let errMsg: string;
+  const error: AxiosError = err as AxiosError;
+
+  const isServerError = !!error.response;
+  const serverUnavailable = !!error.request;
+
+  if (isServerError) {
+    errMsg = (error.response as AxiosResponse<{ error: string }>).data.error;
+  } else if (serverUnavailable) {
+    errMsg = 'Server not available';
+  } else {
+    errMsg = error.message;
+  }
+
+  return errMsg;
+}
