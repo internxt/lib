1817cb7471250c3f81400cb1283ec32bbff8ff8a
Alex Menor <alex.menor@icloud.com>
2022-05-11 10:28:33 +0200

    add testPasswordStrength function

--- src/auth/isValidPassword.ts
+++ src/auth/isValidPassword.ts
@@ -2,7 +2,7 @@
 export const isValidPasswordRegex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,128}$/;
 
 /**
- *
+ * @deprecated Use testPasswordStrength instead
  * @param password Password to test
  * @returns Whether is strong enough according to Internxt's policy
  */
---
+++ test/auth/testPasswordStrength.test.ts
@@ -0,0 +1,59 @@
+import { auth } from '../../src';
+
+const { testPasswordStrength } = auth;
+
+describe('testPasswordStrength tests', () => {
+  it('should consider invalid a password with less than 8 characters', () => {
+    const result = testPasswordStrength('abcd', 'whatever');
+    expect(result.valid).toBe(false);
+
+    if (result.valid === false) {
+      expect(result.reason).toBe('NOT_LONG_ENOUGH');
+    }
+  });
+
+  it('should consider invalid a password with 8 characters but not complex enough', () => {
+    const result = testPasswordStrength('abcdefgh', 'whatever');
+    expect(result.valid).toBe(false);
+
+    if (result.valid === false) {
+      expect(result.reason).toBe('NOT_COMPLEX_ENOUGH');
+    }
+  });
+
+  it('should consider invalid a password with lowercase and uppercase but that looks like his name', () => {
+    const result = testPasswordStrength('PassWord', 'expert_word_user');
+    expect(result.valid).toBe(false);
+
+    if (result.valid === false) {
+      expect(result.reason).toBe('NOT_COMPLEX_ENOUGH');
+    }
+  });
+
+  it('should consider invalid a password with lowercase, uppercase and digits that looks like his name', () => {
+    const result = testPasswordStrength('PassWord4', 'expert_word_user');
+    expect(result.valid).toBe(true);
+
+    if (result.valid) {
+      expect(result.strength).toBe('medium');
+    }
+  });
+
+  it('should consider invalid a password with lowercase, uppercase and symbols that looks like his name', () => {
+    const result = testPasswordStrength('PassWord#', 'expert_word_user');
+    expect(result.valid).toBe(true);
+
+    if (result.valid) {
+      expect(result.strength).toBe('medium');
+    }
+  });
+
+  it('should consider invalid a password with lowercase, uppercase, digits and symbols strong', () => {
+    const result = testPasswordStrength('PassWord#4', 'expert_word_user');
+    expect(result.valid).toBe(true);
+
+    if (result.valid) {
+      expect(result.strength).toBe('hard');
+    }
+  });
+});
