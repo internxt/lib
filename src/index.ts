da32ad0a3428e0b1ebab05b125984933aa98154a
larryrider <larryrider@users.noreply.github.com>
2025-10-30 14:40:42 +0100

    feat: add string utils methods

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import items from './items';
 import auth from './auth';
 import aes from './aes';
 import request from './request';
+import stringUtils from './utils/stringUtils';
 
-export { items, auth, aes, request };
+export { items, auth, aes, request, stringUtils };
